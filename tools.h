3c2ae27a7180d6efeba1441f77e94dd2a50496fd
osakhnatska <43075871+osakhnatska@users.noreply.github.com>
2018-09-29 01:12:42 -0400

    Add files via upload

---
+++ tools.h
@@ -0,0 +1,15 @@
+//function prototypes for tools
+
+#ifndef NAMESPACE_TOOLS_H
+#define NAMESPACE_TOOLS_H
+
+namespace sict {
+
+
+	// Performs a fool-proof integer entry
+	int getInt(int min, int max);
+
+	// Displays the user interface menu
+	int menu();
+}
+#endif // !NAMESPACE_HEADERFILENAME_H
